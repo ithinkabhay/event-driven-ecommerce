7c47ca67088b74939ad99dd1b3ae82e79cbac70b
Abhay Singh <72872402+abhay010918@users.noreply.github.com>
2025-12-11 12:43:52 +0530

    update: Inventory service and fixes bugs on it

--- inventory-service/inventory-service/inventory-service/src/main/java/com/ecommerce/dto/OrderCreatedEvent.java
+++ inventory-service/inventory-service/inventory-service/src/main/java/com/ecommerce/dto/OrderCreatedEvent.java
@@ -1,11 +1,17 @@
 package com.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreatedEvent {
 
     private Long orderId;
@@ -14,7 +20,10 @@ public class OrderCreatedEvent {
     private List<OrderItemEvent> items;
 
     @Data
-    public static class OrderItemEvent{
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OrderItemEvent {
         private Long productId;
         private Integer quantity;
         private BigDecimal price;
